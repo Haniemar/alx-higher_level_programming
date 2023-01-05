259d78eab7050aeac5680e3b52d8e6eae3950e63
Haniemar <haniemar19@gmail.com>
2023-01-05 13:46:09 -0800

    again

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if ord(char) in range(97,123):
+        if ord(char) in range(97, 123):
             c = ord(char) - 32
-            print("{}".format(chr(c)))
+            print("{}".format(chr(c)), end="")
         else:
-            print("{}".format(char))
+            print("{}".format(char), end="")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     number = str(number)
-    print("{}".format(number[-1]),end="")
+    return number[-1]
